ae88a75f3e09d14196086e1884751f74706a64ad
Zhanet <zhanet_f@mail.ru>
2022-09-15 00:00:32 +0300

    fix: add HW6,HW5

--- my-app/src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
+++ my-app/src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import HW7 from "../../h7/HW7";
+import HW8 from "../../h8/HW8";
+import HW9 from "../../h9/HW9";
 
 
 const Junior = () => {
     return (
         <div>
-            Junior
+            <HW7/>
+            <HW8/>
+            <HW9/>
         </div>
     );
 };
