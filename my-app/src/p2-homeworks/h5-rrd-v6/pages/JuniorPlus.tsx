591b8b84b40f6e68e489d140bbb7f13171b66ed5
Zhanet <zhanet_f@mail.ru>
2022-11-02 23:29:44 +0300

    fix:HW11 SuperRange & SuperDoubleRange

--- my-app/src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
+++ my-app/src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import HW11 from "../../h11/HW11";
 
 const JuniorPlus = () => {
     return (
         <div>
-            <HW11/>
+            {/*<HW11/>*/}
         </div>
     );
 };
